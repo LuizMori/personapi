ea5e2e6b61e647c8ba6e0ba9c1204a6238b59338
Luiz Mori <knkmori@hotmail.com>
2022-01-28 20:14:13 -0300

    Initial commit

---
+++ src/test/java/dio/digitalinnovation/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.digitalinnovation.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
